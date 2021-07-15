94835287f92314163a14c2dbdf921380c8e1cdd7
Diorgenes <dptrajano@live.com>
2021-07-15 07:14:59 -0300

    static root

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -124,9 +124,11 @@ STATIC_URL = '/static/'
 
 # Media files
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
